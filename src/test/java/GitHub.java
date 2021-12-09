418d1790090f6f5b36d38b0718f28bc58225cffd
Sümeyra <feza@gmail.com>
2021-12-09 20:59:54 +0100

    2.MAster commit

--- src/test/java/GitHub.java
+++ src/test/java/GitHub.java
@@ -4,7 +4,15 @@ public class GitHub {
     public static void main(String[] args) {
 
         System.out.println("Merhaba Dunya.");
+
+
+        System.out.println("1.Master satir ");
+
+
+
+
     }
+
 
 
 
